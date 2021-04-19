0799d1643348a585ae8486ce8b41fc7e9e2c2f26
caining <cainingning@360.cn>
2021-04-19 16:56:53 +0800

    first commit

---
+++ module01/src/main/java/com/cnn/module01/Module01TestUtils.java
@@ -0,0 +1,14 @@
+package com.cnn.module01;
+
+
+import android.util.Log;
+
+/**
+ * Created by caining on 2021/3/30 11:04
+ * E-Mail Address：cainingning@360.cn
+ */
+public class Module01TestUtils {
+    public static void testModule01(){
+        Log.i("","test");
+    }
+}
---
+++ module02/src/main/java/com/cnn/module02/Module02TestUtils.java
@@ -0,0 +1,16 @@
+package com.cnn.module02;
+
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by caining on 2021/3/30 11:04
+ * E-Mail Address：cainingning@360.cn
+ */
+public class Module02TestUtils {
+    public static void testModule02(Context context){
+        Toast.makeText(context, "module2", Toast.LENGTH_SHORT).show();
+    }
+}
+
