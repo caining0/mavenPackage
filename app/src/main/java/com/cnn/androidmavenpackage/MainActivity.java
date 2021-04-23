893bcf24adc18d86770100db3463616d313aa80d
caining <cainingning@360.cn>
2021-04-23 17:46:55 +0800

    constructor

--- app/src/main/java/com/cnn/androidmavenpackage/MainActivity.java
+++ app/src/main/java/com/cnn/androidmavenpackage/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    public MainActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
